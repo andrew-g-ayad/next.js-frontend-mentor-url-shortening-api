cacd5f75986f049058779e347f369cc874b8d49a
Andrew G. Ayad <anderw_king2010@yahoo.com>
2023-09-03 08:36:42 +0200

    converted AuthLinks to ul

--- components/Layout/Header/AuthLinks/AuthLinks.jsx
+++ components/Layout/Header/AuthLinks/AuthLinks.jsx
@@ -4,20 +4,24 @@ import Link from 'next/link';
 import styles from './AuthLinks.module.scss';
 export default function AuthLinks(props) {
   return (
-    <div className={`${styles.authLinks} ${props.className}`}>
-      <Link
-        href='/login'
-        className={`${styles.btnSecondary} ${styles.btnLogin}`}
-      >
-        Login
-      </Link>
-      <Link
-        href='/register'
-        className={`${styles.btnRegister}`}
-        data-shape='round-borders'
-      >
-        Sign Up
-      </Link>
-    </div>
+    <ul className={`${styles.authLinks} ${props.className}`}>
+      <li>
+        <Link
+          href='/login'
+          className={`${styles.btnSecondary} ${styles.btnLogin}`}
+        >
+          Login
+        </Link>
+      </li>
+      <li>
+        <Link
+          href='/register'
+          className={`${styles.btnRegister}`}
+          data-shape='round-borders'
+        >
+          Sign Up
+        </Link>
+      </li>
+    </ul>
   );
 }
