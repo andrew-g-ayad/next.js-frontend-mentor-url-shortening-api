5339add3c28917bf330a1eef85235b6ee84fb081
Andrew G. Ayad <anderw_king2010@yahoo.com>
2025-03-27 16:43:32 +0200

    Fotter Mob

--- components/Layout/Footer/Footer.jsx
+++ components/Layout/Footer/Footer.jsx
@@ -8,55 +8,57 @@ import ICON_INSTAGRAM from '../../../public/images/icon-instagram.svg';
 import style from './Footer.module.scss';
 export default function Footer() {
   return (
-    <footer className={style.footer}>
-      <div className={style.logo_container}>
-        <Image className={style.logo} src={logo} height={50} width={100} />
-      </div>
-      <ul className={style.ul}>
-        <p className={style.title}>Features</p>
-        <i className={style.li}>Link Shortening</i>
-        <i className={style.li}>Branded Links</i>
-        <i className={style.li}>Analytics</i>
-      </ul>
-      <ul className={style.ul}>
-        <p className={style.title}>Resources</p>
-        <i className={style.li}>Blog</i>
-        <i className={style.li}>Developers</i>
-        <i className={style.li}>Support</i>
-      </ul>
-      <ul className={style.ul}>
-        <p className={style.title}>Company</p>
-        <i className={style.li}>About</i>
-        <i className={style.li}>Our Team</i>
-        <i className={style.li}>Careers</i>
-        <i className={style.li}>Contact</i>
-      </ul>
-      <ul className={style.social_links}>
-        <Image
-          className={style.social_icon}
-          width={50}
-          height={50}
-          src={ICON_FACEBOOK}
-        />
-        <Image
-          className={style.social_icon}
-          width={50}
-          height={50}
-          src={ICON_TWITTER}
-        />
-        <Image
-          className={style.social_icon}
-          width={50}
-          height={50}
-          src={ICON_PINTEREST}
-        />
-        <Image
-          className={style.social_icon}
-          width={50}
-          height={50}
-          src={ICON_INSTAGRAM}
-        />
-      </ul>
-    </footer>
+    <div className={style.background}>
+      <footer className={style.footer}>
+        <div className={style.logo_container}>
+          <Image className={style.logo} src={logo} height={50} width={100} />
+        </div>
+        <ul className={style.ul}>
+          <p className={style.title}>Features</p>
+          <i className={style.li}>Link Shortening</i>
+          <i className={style.li}>Branded Links</i>
+          <i className={style.li}>Analytics</i>
+        </ul>
+        <ul className={style.ul}>
+          <p className={style.title}>Resources</p>
+          <i className={style.li}>Blog</i>
+          <i className={style.li}>Developers</i>
+          <i className={style.li}>Support</i>
+        </ul>
+        <ul className={style.ul}>
+          <p className={style.title}>Company</p>
+          <i className={style.li}>About</i>
+          <i className={style.li}>Our Team</i>
+          <i className={style.li}>Careers</i>
+          <i className={style.li}>Contact</i>
+        </ul>
+        <ul className={style.social_links}>
+          <Image
+            className={style.social_icon}
+            width={50}
+            height={50}
+            src={ICON_FACEBOOK}
+          />
+          <Image
+            className={style.social_icon}
+            width={50}
+            height={50}
+            src={ICON_TWITTER}
+          />
+          <Image
+            className={style.social_icon}
+            width={50}
+            height={50}
+            src={ICON_PINTEREST}
+          />
+          <Image
+            className={style.social_icon}
+            width={50}
+            height={50}
+            src={ICON_INSTAGRAM}
+          />
+        </ul>
+      </footer>
+    </div>
   );
 }
